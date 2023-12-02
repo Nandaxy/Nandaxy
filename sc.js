c29738c9ee24b2c1e2ceb6ef6540364952a4ff35
Nanda <138947445+Nandaxy@users.noreply.github.com>
2023-12-02 20:15:59 +0700

    .

--- sc.js
+++
@@ -1,8 +0,0 @@
-// Check if the screen width is greater than or equal to 768 pixels (desktop)
-if (window.matchMedia("(min-width: 768px)").matches) {
-  // Get the video element
-  const bgVideo = document.getElementById("bgVideo");
-
-  // Change the source of the video
-  bgVideo.src = "src/bg_desktop.mp4";
-}
